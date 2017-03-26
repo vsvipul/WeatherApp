d1daa9ffb2d45db5064a073f3e6219061bf1b2d7
vsvipul555 <vsvipul555@gmail.com>
2017-03-27 00:37:05 +0530

    Added dayname and date functionality

--- js/app.js
+++ js/app.js
@@ -1,13 +1,18 @@
 
-
 $("#submit").click(function () {
 				var cityname=$('#city-name').val();	
               	getData();
             });	
 
 function getData(){
-
 	var cityname=$('#city-name').val();
 	$('#showdata').html('<iframe src="http://api.openweathermap.org/data/2.5/weather?q='+cityname+'&mode=html&appid=4724aeb3679c3b760098a6a700b456fe">alternative content for browsers which do not support iframe.	</iframe>');
-
 }
+
+var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var dayNames = ["Monday", "Tuesday", "Wednesday", "Thurday", "Friday", "Saturday", "Sunday"];
+var date= new Date();
+var day= dayNames[date.getDay()];
+var datemonth= date.getDate()+' '+monthNames[date.getMonth()];
+$('#dayname').html(day)
+$('#datemonth').html(datemonth);
