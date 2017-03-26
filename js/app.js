26d1b87a74696b45a3ca0b9bf07e1d7e9b4536d9
ViPuL <vsvipul555@gmail.com>
2017-03-26 21:40:56 +0530

    Trying fixing iframe

--- js/app.js
+++ js/app.js
@@ -8,6 +8,6 @@ $("#submit").click(function () {
 function getData(){
 
 	var cityname=$('#city-name').val();
-	$('#showdata').html('<iframe src="http://api.openweathermap.org/data/2.5/weather?q='+cityname+'&mode=html&appid=4724aeb3679c3b760098a6a700b456fe">alternative content for browsers which do not support iframe.	</iframe>');
+	$('#showdata').html('<iframe src="http://api.openweathermap.org/data/2.5/weather?q='+cityname+'&mode=html&appid=4724aeb3679c3b760098a6a700b456fe?wmode=opaque">alternative content for browsers which do not support iframe.	</iframe>');
 
 }
